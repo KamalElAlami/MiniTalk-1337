2416fb78a4789f5270f624d0382bbc79abf58945
Mohamed El Bakhkhouch <med.bakhkhouch@gmail.com>
2024-02-26 03:22:33 +0100

    Kunt kan shifti reversed chars xD

--- client.c
+++ client.c
@@ -3,43 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kael-ala <kael-ala@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dedsec <dedsec@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 23:46:46 by kael-ala          #+#    #+#             */
-/*   Updated: 2024/02/23 18:08:16 by kael-ala         ###   ########.fr       */
+/*   Updated: 2024/02/26 03:07:37 by dedsec           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-void send_sig(pid_t pid, char c)
+void send_sig(pid_t pid, char *str)
 {
     int i;
     
-    i = 7;
-    while (i >= 0)
-    {
-        if ((c >> i) & 1)
-            // printf("1");
-            kill(pid, SIGUSR1);
-        else
-            // printf("0");
-            kill(pid, SIGUSR2);
-        i--;
+    while (*str)
+    {        
+        i = 0;
+        while (i <= 7)
+        {
+            if ((*str >> i) & 1)
+            {
+                printf("1");
+                kill(pid, SIGUSR1);
+            }
+            else
+            {
+                printf("0");
+                kill(pid, SIGUSR2);
+            }
+            i++;
+            usleep(500);
+        }
+        *str++;
     }
 }
 
 int main(int ac, char **av)
 {
-    int i;
+    pid_t pidd;
 
-    i = 0;
+    pidd = atoi(av[1]);
     if (ac == 3)
     {
-        while (av[2][i])
-        {
-            send_sig(atoi(av[1]), av[2][i]);
-            i++;
-        }
+        send_sig(pidd, av[2]);
     }
 }
--- server.c
+++ server.c
@@ -7,26 +7,21 @@
 
 void sig_handler(int sig)
 {
-	int sigg;
-	int i;
+	static int sigg;
+	static int i = 0;
 
-	i = 7;
-
-	while (i >= 7)
+	if (sig == SIGUSR1)
 	{
-		if (sig == SIGUSR1)
-		{
-			sigg |= (1 << i);
-			i++;
-		}
+		sigg |= (1 << i);
 	}
+
+	i++;
 	if (i == 8)
 	{
 		write(1, &sigg, 1);
 		i = 0;
 		sigg = 0;
 	}
-
 }
 int main(void)
 {
@@ -39,6 +34,7 @@ int main(void)
 	{
 		signal(SIGUSR1, sig_handler);
 		signal(SIGUSR2, sig_handler);
+		pause();
 	}
 
 }
